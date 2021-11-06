10701ded72d3c53624c17ad99bbacf458a9086f7
Vlad <me@vlad.ai>
2021-11-06 15:19:12 -0700

    started readme, limiting by ip

--- index.js
+++ index.js
@@ -9,30 +9,22 @@ const app = express();
 
 const limit = rateLimiter.create({
   redis: redisClient,
-  key: 'ip',
+  key: 'ip', // function(x) { return x.headers['user-agent']; },
   rate: '10/minute'
 });
 
 app.get('/check', (req, res) => {
   limit(req, function(err, rate) {
     if (err) {
-      console.warn('Rate limiting not available');
+      res.send(500);
     } else {
-      const responsePayload = {
+      res.json({
         approachingOver: utils.approachingOver(rate.current, rate.limit),
         current: rate.current,
         over: rate.over,
-      };
-      if (responsePayload.approachingOver && !responsePayload.over) {
-        console.error('--> approaching the limit!');
-      }
-      if (responsePayload.over) {
-        console.error('too many requesets!');
-        res.status(429, 'too many requests');
-      }
-    }
+      });
+    };
   });
-  res.send(200, 'OK');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("starting!"));
+app.listen(process.env.PORT || 3000, () => console.log('\n\tStarted!'));
--- utils/misc.js
+++ utils/misc.js
@@ -3,7 +3,7 @@ const APPROACHING_OVER_RATIO = 0.8;
 const approachingOver = (current, limit) => {
   const currentRatio = (current / limit).toPrecision(2);
   console.log('currentRatio', currentRatio, current, limit);
-  return currentRatio >= APPROACHING_OVER_RATIO
+  return currentRatio >= APPROACHING_OVER_RATIO || false
 }
 
 exports.approachingOver = approachingOver;
